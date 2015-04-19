678800774966d05dcace64de7223126b89f618b6
jameshong <jameshongbiz333@gmail.com>
2015-04-19 15:27:15 +0900

    Initial Commit

---
+++ self-sizing-cell-ios8Tests/self_sizing_cell_ios8Tests.swift
@@ -0,0 +1,36 @@
+//
+//  self_sizing_cell_ios8Tests.swift
+//  self-sizing-cell-ios8Tests
+//
+//  Created by JH on 4/19/15.
+//  Copyright (c) 2015 mushroomapps. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class self_sizing_cell_ios8Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
